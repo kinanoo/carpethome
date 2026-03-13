74600b195fb411d69b6a3ed5edc07812c2c86b58
kinanoo <143280296+kinanoo@users.noreply.github.com>
2026-03-14 02:18:49 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,12 +21,6 @@ export default defineConfig({
       },
     },
     chunkSizeWarningLimit: 1000,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    minify: 'esbuild',
   },
 })
